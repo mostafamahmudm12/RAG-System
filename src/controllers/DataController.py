21bba177ea1e259dee81979327caf43ad9b51e57
Mostafa Mahmoud <mostafamahmudm13@gmail.com>
2026-02-20 04:26:12 +0200

    update

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -24,7 +24,7 @@ class DataController(BaseController):
 
 
 
-    def generate_unique_filename(self,original_filename: str, project_id: str):
+    def generate_unique_filepath(self,original_filename: str, project_id: str):
 
         random_filename=self.generate_random_string()
         project_path=ProjectController().get_project(project_id)
@@ -43,7 +43,7 @@ class DataController(BaseController):
             random_filename + "_" + clean_filename
             )
 
-        return new_file_path
+        return new_file_path ,random_filename + "_" + clean_filename
     
 
 
--- src/models/enums/ResponseEnum.py
+++ src/models/enums/ResponseEnum.py
@@ -7,3 +7,5 @@ class ResponseSignal(Enum):
     FILE_SIZE_EXCEEDED = "File size exceeds the maximum allowed size"
     FILE_UPLOAD_SUCCESS = "File uploaded successfully"
     FILE_UPLOAD_FAILED = "File upload failed"
+    PROCCESSING_FAILED = "File processing failed"
+    PROCCESSING_SUCCESS = "File processed successfully"
