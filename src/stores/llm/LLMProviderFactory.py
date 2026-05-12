d0556dd55bc409484c6025f999f4e29eda98d923
Mostafa Mahmoud <mostafamahmudm13@gmail.com>
2026-05-12 18:44:54 +0300

    create llm provider factory

---
+++ src/stores/llm/LLMProviderFactory.py
@@ -0,0 +1,28 @@
+from .LLMEnums import LLMEnum
+from .providers import OpenAiProvider, CohereProvider
+
+class LLMProviderFactory:
+    def __init__(self, config: dict):
+        self.config = config
+
+    def create(self, Provider: str):
+        
+        if Provider == LLMEnum.OPENAI.value:
+            return OpenAiProvider(
+                api_key=self.config.OPENAI_API_KEY,
+                api_url=self.config.OPENAI_API_URL,
+                defult_input_max_characters=self.config.INPUT_DAFAULT_MAX_CHARACTERS,
+                defult_generation_max_output_tokens=self.config.GENERATION_DEFULT_MAX_TOKENS,
+                defult_generation_temperature=self.config.GENERATION_DEFULT_TEMPERATURE,
+
+            )
+
+        if Provider == LLMEnum.COHERE.value:
+            return CohereProvider(
+                api_key=self.config.COHERE_API_KEY,
+                defult_input_max_characters=self.config.INPUT_DAFAULT_MAX_CHARACTERS,
+                defult_generation_max_output_tokens=self.config.GENERATION_DEFULT_MAX_TOKENS,
+                defult_generation_temperature=self.config.GENERATION_DEFULT_TEMPERATURE,
+            )
+
+        
