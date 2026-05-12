efb27a327ca88cb455975ce67cdfe02ccc14892c
Mostafa Mahmoud <mostafamahmudm13@gmail.com>
2026-05-12 18:43:48 +0300

    update requirements and env example and config

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -10,6 +10,19 @@ class Settings(BaseSettings):
     MONGO_URL: str
     MONGO_DATABASE: str
 
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY : str= None
+    OPENAI_API_URL : str= None
+    COHERE_API_KEY : str= None
+    GENERATION_MODEL_ID : str= None
+    EMBEDDING_MODEL_ID : str= None
+    EMBEDDING_MODEL_SIZE : int= None
+    INPUT_DAFAULT_MAX_CHARACTERS : int= None
+    GENERATION_DEFULT_MAX_TOKENS : int= None
+    GENERATION_DEFULT_TEMPERATURE : float= None 
+
     class Config:
         env_file = ".env"
     
