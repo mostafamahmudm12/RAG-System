69c5ab841a5d542c8efd5af6ecb8714d126fa7e3
Mostafa Mahmoud <mostafamahmudm13@gmail.com>
2026-05-12 18:45:24 +0300

    update main and LLM enums

--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -10,3 +10,15 @@ class OpenAIEnum(Enum):
     SYSTEM="system"
     USER="user"
     ASSISTANT="assistant"
+
+
+class CohereEnum(Enum):
+    SYSTEM="SYSTEM"
+    USER="USER"
+    ASSISTANT="CHATBOT"
+    DOCUMENT="search_document"
+    QUERY="search_query"
+
+class DocumentTypeEnum(Enum):
+    DOCUMENT = "document"
+    QUERY = "query"
